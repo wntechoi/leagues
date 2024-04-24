cc0bb193ad124a3cb9d58a2a12cb02abcbac4356
wntechoi <167000818+wntechoi@users.noreply.github.com>
2024-04-24 21:25:14 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -78,7 +78,7 @@ if st.sidebar.button('플레이어 랭킹 보기'):
 	ascend = False if order not in  ['플레이어', '평균 데스'] else True
 	data = data.sort_values(by=[order], axis=0, ascending=ascend).reset_index(drop=True)
 	style = {}
-	for column in data.columns:
+	for column in data.select_dtypes(include=['float']).columns:
 	    style[column] = "{:.2f}".format
 	formatted_df = data.style.format(style)
 	st.table(formatted_df)
@@ -101,7 +101,7 @@ if st.sidebar.button('플레이어 스탯 보기'):
 	ascend = False if order_stat not in  ['챔피언', '평균 데스'] else True
 	data = data.sort_values(by=[order_stat], axis=0, ascending=ascend).reset_index(drop=True)
 	style = {}
-	for column in data.columns:
+	for column in data.select_dtypes(include=['float']).columns:
 	    style[column] = "{:.2f}".format
 	formatted_df = data.style.format(style)
 	st.table(formatted_df)
@@ -125,7 +125,7 @@ if st.sidebar.button('매치업 보기'):
 	data = data[['position_x', 'champion_x', 'kill_x', 'death_x', 'assist_x','cs_x','damage_dealt_x', 'sight_score_x', 'win_or_not_x','game_time_x', 'win_or_not_y', 'position_y', 'champion_y', 'kill_y', 'death_y', 'assist_y','cs_y','damage_dealt_y', 'sight_score_y']]
 	data.columns = [f'포지션_{player1}', f'챔피언_{player1}', f'킬_{player1}', f'데스_{player1}', f'어시스트_{player1}', f'cs_{player1}',f'딜량_{player1}', f'시야점수_{player1}', f'승리여부_{player1}','게임 시간',f'승리여부_{player2}',f'포지션_{player2}', f'챔피언_{player2}', f'킬_{player2}', f'데스_{player2}', f'어시스트_{player2}', f'cs_{player2}',f'딜량_{player2}', f'시야점수_{player2}']
 	style = {}
-	for column in data.columns:
+	for column in data.select_dtypes(include=['float']).columns:
             style[column] = "{:.2f}".format
 	formatted_df = data.style.format(style)
 	st.table(formatted_df)
@@ -147,7 +147,7 @@ if st.sidebar.button('팀 궁합 보기'):
 	data = data[['win_or_not_x','position_x', 'champion_x', 'kill_x', 'death_x', 'assist_x','cs_x','damage_dealt_x', 'sight_score_x','game_time_x', 'position_y', 'champion_y', 'kill_y', 'death_y', 'assist_y','cs_y','damage_dealt_y', 'sight_score_y']]
 	data.columns = [f'승리여부',f'포지션_{p1}', f'챔피언_{p1}', f'킬_{p1}', f'데스_{p1}', f'어시스트_{p1}', f'cs_{p1}',f'딜량_{p1}', f'시야점수_{p1}', '게임 시간',f'포지션_{p2}', f'챔피언_{p2}', f'킬_{p2}', f'데스_{p2}', f'어시스트_{p2}', f'cs_{p2}',f'딜량_{p2}', f'시야점수_{p2}']
 	style = {}
-	for column in data.columns:
+	for column in data.select_dtypes(include=['float']).columns:
 	    style[column] = "{:.2f}".format
 	formatted_df = data.style.format(style)
 	st.table(formatted_df)
